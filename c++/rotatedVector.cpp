5fc8b718a6eaf3db78230c9b8916d283c535255e
Minh Le <jst.mle@gmail.com>
2014-12-18 08:50:31 -0500

    update

--- c++/rotatedVector.cpp
+++ c++/rotatedVector.cpp
@@ -2,58 +2,69 @@
 #include <vector>
 #include <cassert>
 #include <algorithm>
+#include <iterator>
 
 using namespace std;
+typedef vector<int> vi;
+typedef unsigned uint;
 
 int findRotationCount (const vector<int>& v);
 
-void printv(const vector<int>& v) 
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const vector<T>& v)
 {
-   for(size_t i=0; i<v.size();++i) 
-      cout<<v[i]<<" ";
-    cout<<endl;           
-} 
+    for (auto it=v.cbegin(); v.cend(); ++it)
+        os << "[" << *it << "] ";
 
-/*Given a rotated sorted array and count 'k' by which the array 
-is rotated clockwise is not known, find Nth highest number. Assume that array was sorted in ascending order before rotation. For finding minimum element pass 'n' as 1 and for finding maximum pass 'n' as size.*/
+    return os;
+}
+
+
+/***
+ * Given a rotated sorted array and count 'k' by which the array is 
+ * rotated clockwise is not known, find Nth highest number. Assume 
+ * that array was sorted in ascending order before rotation. For 
+ * finding minimum element pass 'n' as 1 and for finding maximum 
+ * pass 'n' as size.
+ * */
 
 int findNthHighest(const vector<int>& v, int N) 
 {
-  assert(N>0 && N<static_cast<int>(v.size()) );
- int count = findRotationCount(v);
- return v[v.size() - count -  N];
+    assert(N>0 && N<static_cast<int>(v.size()) );
+    int count = findRotationCount(v);
+    return v[v.size() - count -  N];
 } 
 
 // 456789123 lo=4 hi=3 mid=8
 // 9123 lo=9 hi=3 mid=1
-/*Given a rotated sorted array find the count by which the array is rotated. Array was sorted in ascending order before rotation and was rotated clockwise*/
+/**
+ * Given a rotated sorted array find the count by which the array 
+ * is rotated. Array was sorted in ascending order before rotation 
+ * and was rotated clockwise
+ **/
 int findRotationCount (const vector<int>& v) 
 {
-   int k=v.size() - 1, count=0;
-   while (k>0) {
-     if (v[k-1]>v[k]) break;
-     --k;
-     ++count;
+    int k=v.size() - 1, count=0;
+    while (k>0) {
+        if (v[k-1]>v[k]) break;
+        --k;
+        ++count;
 
-  } 
-   return count+1;              
+    } 
+    return count+1;              
 } 
 
 void reverseVector(vector<int>& v, int lo, int hi) 
 {
-  for(int i=lo,j=hi;i<=(lo+hi)/2;++i,--j) {
-    swap(v[i],v[j]);
- }                    
+    for(int i=lo,j=hi;i<=(lo+hi)/2;++i,--j) {
+        swap(v[i],v[j]);
+    }                    
 } 
 void rotateVector (vector<int>&v, int k)
 {
-  printv(v);
-  reverseVector(v,0,v.size()-1);
-   printv(v);
-   reverseVector(v,0,v.size()-k-1);
-    printv(v);
+    reverseVector(v,0,v.size()-1);
+    reverseVector(v,0,v.size()-k-1);
     reverseVector(v,v.size()-k,v.size()-1);   
-    printv(v);  
 } 
 
 /**
@@ -65,14 +76,14 @@ int findRotationCount_bsearch (const vector<int>& v, int lo, int hi)
 {
     uint mid = (lo + hi) / 2;
 
-    cout<<"\nmid = " << mid << "  v[mid]=" << v[mid] << "  v[lo]=" << v[lo]<< "  v[hi]=" << v[hi];
     if (mid==lo || mid==hi) 
         return v[lo]>v[hi] ? hi : lo;
     else if (v[mid] == v[hi] && v[mid] == v[lo]) {
+    // To fix the failure mentioned above, recursively passing
+    // both sides of the array and return the index
         int from_lo = findRotationCount_bsearch (v, lo, mid);
         int from_hi = findRotationCount_bsearch (v, mid+1, hi);
-        
-        cout<<"\nfrom_lo = " << from_lo << "\tfrom_hi = " << from_hi << endl;
+
         return v[from_lo]>v[from_hi] ? from_hi : from_lo;
     } else if (v[mid] > v[hi]) {
         return findRotationCount_bsearch (v, mid, hi);
@@ -83,20 +94,24 @@ int findRotationCount_bsearch (const vector<int>& v, int lo, int hi)
 
 void test_rotateVector () 
 {
-  vector<int>v;
-  int arr[] = {4,5,6,7,8,9,1,2,3};
+    vector<int>v;
+    int arr[] = {4,5,6,7,8,9,1,2,3};
 
-  for(int i=0;i<9;++i) v.push_back(arr[i]);
-  cout<<"3rd largest is " << findNthHighest(v, 3) <<endl;
-  cout<<"the largest is " << findNthHighest(v,1)<<endl;
+    for(int i=0;i<9;++i) v.push_back(arr[i]);
+    //cout<<"3rd largest is " << findNthHighest(v, 3) <<endl;
+    //cout<<"the largest is " << findNthHighest(v,1)<<endl;
 
-  v.clear();
-  srand(time(NULL));
-  for (int i=0; i<15;++i) v.push_back(rand() %53);
-  sort(v.begin(), v.end());
-  rotateVector(v, 4);
+    v.clear();
+    //srand(time(NULL));
+    //for (int i=0; i<15;++i) v.push_back(rand() %53);
+    //sort(v.begin(), v.end());
+    //rotateVector(v, 4);
 
-  cout<<"rotation cnt = "<<findRotationCount(v);
-  cout<<"the largest is " << findNthHighest(v,3)<<endl;
+    //cout<<"rotation cnt = "<<findRotationCount(v);
+    //cout<<"the largest is " << findNthHighest(v,3)<<endl;
+
+    vi v1{4,4,4,4,4,4,2,3,4,4};
+    int idx = findRotationCount_bsearch (v1, 0, v1.size()-1);
+    cout<<"\nCount using bsearch: " << (v1.size() - idx) << endl;
 
 } 
