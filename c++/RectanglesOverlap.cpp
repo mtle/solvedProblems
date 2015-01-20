70a7d2bc40b9d48839c5884e06bb63bc8658c541
mtle <mtle@users.noreply.github.com>
2015-01-20 14:10:06 -0500

    Update RectanglesOverlap.cpp

--- c++/RectanglesOverlap.cpp
+++ c++/RectanglesOverlap.cpp
@@ -2,10 +2,6 @@
   Given two axis-aligned rectangles A and B. Write a function to determine 
   if the two rectangles overlap.
 
-Hint:
-If you are coming up with a complicated set of conditionals, you might think 
-too hard. There is an easier way. Try to think in the opposite direction.
-
 */
 #include <iostream>
 #include <sstream>
